ed21630ff6038daa2d3d66abdebbb41e8aaf7bcb
Gennadion <ghammurti@gmail.com>
2021-05-28 09:15:07 +0300

    Add project files.

---
+++ FinalExam/FinalExam.cpp
@@ -0,0 +1,25 @@
+/** \file FinalExam.cpp
+ *  \brief     A little program to manipulate array of integers.
+ *  \details   The program reads into the array, sums all elements and makes all elements 0.
+ *  \author    Gennady Petrishchev
+ *  \version   0.1
+ *  \date      2020-2021
+ *  \pre       First initialize the system.
+ *  \bug       No bugs so far
+ *  \copyright University of Nicosia.
+ */
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+/**
+ * The <code>main</code> is the driver function.
+ * <BR>
+ * @return Returns <code>0</code> upon successful execution and other value if otherwise.
+ */
+int main()
+{
+ 
+}
+
